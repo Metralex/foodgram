fa279f654299a727765a49ea0b7dfb75c1b4a395
metralex <art010600@yandex.ru>
2025-11-14 15:15:16 +0300

    FileResponse removed

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
