8d6f7c2d41471fcfa70f8a3b331944fe875270f4
metralex <art010600@yandex.ru>
2025-11-19 00:31:46 +0300

    useles import remove

--- backend/recipes/management/commands/import_tags.py
+++ backend/recipes/management/commands/import_tags.py
@@ -6,7 +6,6 @@ from typing import Any
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandParser
-from django.utils.text import slugify
 from recipes.models import Tag
 
 
